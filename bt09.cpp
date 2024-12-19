8231ea8e1e1348952e08ee62d3a1c6bdc8459fb0
thanhlong2023 <135001326+thanhlong2023@users.noreply.github.com>
2024-12-19 14:42:17 +0700

    Add files via upload

--- bt09.cpp
+++ bt09.cpp
@@ -1,36 +1,41 @@
 #include <bits/stdc++.h>
+using namespace std;
+
 int dem[1001];
 #define lenght sizeof(arr) / sizeof(int)
-#define lenghtDem sizeof(dem) / sizeof(int)
+
 int main()
 {
     int arr[] = {1, 5, 2, 3, 23, 5, 1, 2};
+    int n = lenght;
     int max = -1e9;
-    for (int i = 0; i < lenght; i++)
+
+    for (int i = 0; i < n; i++)
     {
         dem[arr[i]]++;
         if (arr[i] > max)
             max = arr[i];
     }
+
     int cnt = -1e9;
-    int mostFrequent = -1;
     for (int i = 0; i <= max; i++)
     {
-        // printf("%d ", dem[i]);
         if (dem[i] > cnt)
         {
             cnt = dem[i];
         }
     }
-    for (int i = 0; i <= max; i++)
-    {
 
-        if (dem[i] == cnt)
+    int tanSuat = -1;
+    for (int i = 0; i < n; i++)
+    {
+        if (dem[arr[i]] == cnt)
         {
-            printf("%d ", i);
+            tanSuat = arr[i];
             break;
         }
     }
 
+    printf("%d\n", tanSuat);
     return 0;
 }
